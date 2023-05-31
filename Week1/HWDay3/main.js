b1ea245919317f41fa13e5486dc8a975963a133a
sim21kaur <102401699+sim21kaur@users.noreply.github.com>
2023-05-31 07:26:44 -0400

    Update main.js

--- Week1/HWDay3/main.js
+++ Week1/HWDay3/main.js
@@ -13,3 +13,23 @@ function get10FibNums() {
 }
 
 console.log(get10FibNums());
+
+function print_fibonacci_sequence(){
+    num1 = 0;
+    num2 = 1;
+    nextTerm = num1 + num2;
+    console.log(num1);
+    console.log(num2);
+
+    for(let i = 3; i <= 10; i++){
+        console.log(nextTerm);
+        num1 = num2;
+        num2 = nextTerm;
+        nextTerm = num1 + num2;
+    }
+}
+
+print_fibonacci_sequence();
+
+
+
