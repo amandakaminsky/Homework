01a18428eff8f1be4e5bd0442fdca99557357a45
amandakaminsky <76925225+amandakaminsky@users.noreply.github.com>
2023-06-07 10:08:42 +0200

    Add files via upload

---
+++ Week2:HWDay1:src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BookForm from './components/BookForm';
+import BookTable from './components/BookTable';
+
+function App() {
+  const [books, setBooks] = useState([]);
+  const [bookToEdit, setBookToEdit] = useState(null);
+
+  function createBook(book) {
+    setBookToEdit(null);
+    setBooks([...books, book]);
+  }
+
+  function deleteBook(book) {
+    setBooks(books.filter((x) => x.isbn !== book.isbn));
+  }
+
+  function editBook(book) {
+    setBookToEdit(book);
+    setBooks(books.filter((x) => x.isbn !== book.isbn));
+  }
+
+  return (
+    <div className = "text-center m-5">
+      <div className = "card p-5">
+        <BookForm bookToEdit = {bookToEdit} createBook = {createBook} > </BookForm>
+        <BookTable books = {books} editBook = {editBook} deleteBook = {deleteBook}> </BookTable>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week2:HWDay2:src/models/Pokemon.js
@@ -0,0 +1,7 @@
+export class Pokemon {
+    constructor(name, abilities, rarity) {
+        this.name = name;
+        this.abilities = abilities;
+        this.rarity = rarity;
+    }
+}
